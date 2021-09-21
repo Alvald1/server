94729e2b7c6341acd43837d369d7b17d1d590df6
Afanasyy <66055931+Afanasyy@users.noreply.github.com>
2021-09-21 20:04:44 +0300

    1

---
+++ public/index.js
@@ -0,0 +1,51 @@
+function get(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+
+  xhr.onload = function () {
+    //let val = document.getElementById("elem1").value;
+    var val = xhr.response;
+    let json = JSON.parse(val);
+    let keys = Object.keys(json[0]);
+    console.log(keys);
+    setTableNames(keys);
+    setTableVal(json);
+    // Запрос завершён. Здесь можно обрабатывать результат.
+  };
+
+  xhr.send(null);
+}
+
+butt.onclick = function () {
+  let val = document.getElementById("elem1").value;
+  get("/getMarks?name=" + val);
+};
+let flag = true;
+function setTableNames(keys) {
+  let tab = document.getElementById("tb");
+  let row = document.createElement("tr");
+  for (let v in keys) {
+    let tmp = document.createElement("td");
+    let text = document.createTextNode(keys[v]);
+    tmp.appendChild(text);
+    row.appendChild(tmp);
+  }
+  tab.appendChild(row);
+  row.style.backgroundColor = "silver";
+}
+function setTableVal(arr) {
+  let tab = document.getElementById("tb");
+  for (let i in arr) {
+    let row = document.createElement("tr");
+    for (let j in arr[i]) {
+      let col = document.createElement("td");
+      let text = document.createTextNode(arr[i][j]);
+      col.appendChild(text);
+      row.appendChild(col);
+    }
+    tab.appendChild(row);
+    if (flag) row.style.backgroundColor = "dimgrey";
+    else row.style.backgroundColor = "silver";
+    flag = !flag;
+  }
+}
